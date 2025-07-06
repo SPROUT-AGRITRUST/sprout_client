ce90b6b3a23df9ccda09dfbe87c9ec589c0ade70
GurunadaRao <gurunadharao5718@gmail.com>
2025-07-06 09:38:22 +0530

    Initiated Git  repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl  text-amber-600  text-center ">AgriNext </h1>
+    </div>
+  );
+};
+
+export default App;
